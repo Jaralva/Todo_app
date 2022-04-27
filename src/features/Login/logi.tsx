12af7622a1246734738d0b6bc72cba38cd6d13d4
Jaralva <zaingrazes@mail.ru>
2022-04-27 20:26:00 +0300

    Initial commit

---
+++ src/features/Login/logi.tsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import Grid from '@mui/material/Grid';
+import Checkbox from '@mui/material/Checkbox';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import FormLabel from '@mui/material/FormLabel';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import {useFormik} from "formik";
+import {useDispatch, useSelector} from "react-redux";
+import {loginTC} from "./auth_reducer";
+import {AppRootStateType} from "../../app/store";
+import { Navigate } from 'react-router-dom';
+import {LoginParamsType} from "../../api/todolists-api";
+
+type FormikErrorType = {
+    email?: string
+    password?: string
+    rememberMe?: boolean
+}
+
+
+export const Login = () => {
+    const dispatch = useDispatch()
+
+    const isLoginedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        validate: (values) => {
+            const errors: Partial<Omit<LoginParamsType, 'captcha'>> = {};
+            if (!values.email) {
+                errors.email = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+            if (!values.password) {
+                errors.password = "Required"
+            }
+            else if ( values.password.length <= 4){
+                errors.password = 'Password is short'
+            }
+            return errors;
+        },
+        onSubmit: values => {
+            dispatch(loginTC({email: values.email, password: values.password, rememberMe: undefined, captcha: undefined}))
+        },
+    })
+
+    if (isLoginedIn) {
+        return <Navigate to={"/"} />
+    }
+
+
+    return <Grid container justifyContent={'center'}>
+        <Grid item justifyContent={'center'}>
+
+            <FormControl>
+                <FormLabel>
+                    <p>To log in get registered
+                        <a href={'https://social-network.samuraijs.com/'}
+                           target={'_blank'}> here
+                        </a>
+                    </p>
+                    <p>or use common test account credentials:</p>
+                    <p>Email: free@samuraijs.com</p>
+                    <p>Password: free</p>
+                </FormLabel>
+                <form onSubmit={formik.handleSubmit}>
+                <FormGroup>
+                    <TextField label="Email"
+                               margin="normal"
+                        {...formik.getFieldProps('email')}
+                    />
+                    {formik.touched.email && formik.errors.email && <div style={{color : 'red'}}>{formik.errors.email}</div>}
+                    <TextField type="password"
+                               label="Password"
+                               margin="normal"
+                               {...formik.getFieldProps('password')}
+                    />
+                    {formik.touched.password && formik.errors.password && <div style={{color: 'red'}}>{formik.errors.password}</div>}
+                    <FormControlLabel label={'Remember me'} control={<Checkbox/>}
+                                      {...formik.getFieldProps('rememberMe')}/>
+                    <Button type={'submit'} variant={'contained'} color={'primary'}>
+                        Login
+                    </Button>
+                </FormGroup>
+            </form>
+            </FormControl>
+
+        </Grid>
+    </Grid>
+}
